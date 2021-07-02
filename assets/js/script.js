8a77165b09de59a716e2ad6890cc8792f29c0e73
Diana Salas <salasd10418@gmail.com>
2021-07-01 23:48:23 -0400

    initial commmit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// startBtn onclick **** need to add eventListener
+    // timer starts ===> present question
+
+// answer question
+    // present new question **** iteration for loop
+
+// answer question incorrectly
+    // time-- from clock **** var subtractTime = 5
+        // all questions answered === true || timer === 0
+        // ********* timer can't go negative Math.max?
+            // game over
+                // save initials and score
