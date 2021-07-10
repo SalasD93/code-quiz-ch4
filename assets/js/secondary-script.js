d7e27d5b7e889b74d5660193478b4142b164e576
Diana Salas <salasd10418@gmail.com>
2021-07-09 22:48:31 -0400

    add local storage and write it to page / add function to clear score button

--- assets/js/secondary-script.js
+++ assets/js/secondary-script.js
@@ -10,6 +10,7 @@ var clearBtn = document.createElement('button');
 
 highScoreContainer.setAttribute('style', 'display: flex; flex-wrap: wrap; flex-direction: column; align-items: flex-start;  margin-top: 10%; margin-left: 35%; font-family: Arial, Helvetica, sans-serif;');
 hsList.setAttribute('style', 'background: #e8f0fc; padding: 1% 50% 1% 3%; margin-top: 2%;');
+hsItem.setAttribute('id', "high-score");
 buttonsEl.setAttribute('style', 'display: flex; flex-wrap: wrap; justify-content: flex-start; width: 40%; margin: 2% 0;');
 backBtn.setAttribute('style', 'width: 20%; padding: 1% 0; border: none; border-radius: 5px; background: #0062ff; color: #fff;');
 clearBtn.setAttribute('style', 'width: 40%; padding: ; margin-left: 2%; border: none; border-radius: 5px; background: #0062ff; color: #fff;');
@@ -27,8 +28,17 @@ highScoreContainer.appendChild(buttonsEl);
 buttonsEl.appendChild(backBtn);
 buttonsEl.appendChild(clearBtn);
 
+var highScore = localStorage.getItem("highscore");
+// document.getElementById("high-score").innerHTML = highScore;
+var initials = localStorage.getItem("initials");
+document.getElementById("high-score").innerHTML = initials + " - " + highScore;
+// document.getElementById("high-score").innerHTML
+
 backBtn.addEventListener('click', function() {
     window.location= "./index.html";
 });
 
+clearBtn.addEventListener('click', function() {
+    document.getElementById("high-score").innerHTML = '';
+});
 // get local storage initials and score append to li
